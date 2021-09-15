87f278b111a23633621b3c139286817ee181924d
JunChi <jun@JunChis-MacBook-Pro.local>
2021-09-15 16:06:14 +0800

    added eslint and created function to upload files

--- main.js
+++ main.js
@@ -1,59 +1,99 @@
-Moralis.initialize("l8T3PYmGe5rBWzNswX90jSlbQ9p7kjxpznHHjPBO");
-Moralis.serverURL = 'https://t4jve9pvn40e.grandmoralis.com:2053/server'
+/* eslint-disable max-len */
+/* eslint-disable require-jsdoc */
+Moralis.initialize('l8T3PYmGe5rBWzNswX90jSlbQ9p7kjxpznHHjPBO');
+Moralis.serverURL = 'https://t4jve9pvn40e.grandmoralis.com:2053/server';
 
 // enable moralis and connect to web3
 init = async () => {
-    hideElement(userInfo)
-    window.web3 = await Moralis.web3.enable();
-    initUser();
-}
+  hideElement(userInfo);
+  window.web3 = await Moralis.web3.enable();
+  initUser();
+};
 
 login = async () => {
-    try {
-        await Moralis.authenticate();
-        console.log("logged in")
-        initUser();
-    } catch (error) {
-        alert(error)
-    }
-}
+  try {
+    await Moralis.authenticate();
+    console.log('logged in');
+    initUser();
+  } catch (error) {
+    alert(error);
+  }
+};
 
 async function logOut() {
-    await Moralis.User.logOut();
-    hideElement(userInfo)
-    initUser()
-    console.log("logged out");
-  }
+  await Moralis.User.logOut();
+  hideElement(userInfo);
+  initUser();
+  console.log('logged out');
+}
 
 openUserInfo = async () => {
-    user = await Moralis.User.current()
-    if (user) {
-        showElement(userInfo)
+  user = await Moralis.User.current();
+  if (user) {
+    const email = user.get('email');
+    if (email) {
+      userEmailField.value = email;
     } else {
-        login()
+      userEmailField.value = '';
     }
-} 
+
+    userUsernameField.value = user.get('username');
+
+    const userAvatar = user.get('avatar');
+    if (userAvatar) {
+      userAvatarImg.src = userAvatar.url();
+      showElement(userAvatarImg);
+    } else {
+      hideElement(userAvatarImg);
+    }
+
+    showElement(userInfo);
+  } else {
+    login();
+  }
+};
+
+saveUserInfo = async () => {
+  user.set('email', userEmailField.value);
+  user.set('username', userUsernameField.value);
+
+
+  if (userAvatarFile.files.length > 0) {
+    const avatar = new Moralis.File('avatar.jpg', userAvatarFile.files[0]);
+    user.set('avatar', avatar);
+  }
+
+  await user.save();
+  alert('user info saved successfully');
+  openUserInfo();
+};
 
 initUser = async () => {
-    if (await Moralis.User.current()) {
-        hideElement(userConnectButton)
-        showElement(userProfileButton)
-} else {
-    showElement(userConnectButton)
-    hideElement(userProfileButton)
-    }
-}
-hideElement = (element) => element.style.display = "none";
-showElement = (element) => element.style.display = "block"
+  if (await Moralis.User.current()) {
+    hideElement(userConnectButton);
+    showElement(userProfileButton);
+  } else {
+    showElement(userConnectButton);
+    hideElement(userProfileButton);
+  }
+};
+hideElement = (element) => element.style.display = 'none';
+showElement = (element) => element.style.display = 'block';
 
-const userConnectButton = document.getElementById("btnConnect")
+const userConnectButton = document.getElementById('btnConnect');
 userConnectButton.onclick = login;
 
-const userProfileButton = document.getElementById("btnUserInfo")
+const userProfileButton = document.getElementById('btnUserInfo');
 userProfileButton.onclick = openUserInfo;
 
-const userInfo = document.getElementById("userInfo")
-document.getElementById("btnCloseUserInfo").onclick = () => hideElement(userInfo)
-document.getElementById("btnLogout").onclick = logOut
+const userInfo = document.getElementById('userInfo');
+const userUsernameField = document.getElementById('txtUsername');
+const userEmailField = document.getElementById('txtEmail');
+const userAvatarImg = document.getElementById('imgAvatar');
+const userAvatarFile = document.getElementById('fileavatar');
 
-init()
+document.getElementById('btnCloseUserInfo').onclick = () => hideElement(userInfo);
+document.getElementById('btnLogout').onclick = logOut;
+document.getElementById('btnSaveUserInfo').onclick = saveUserInfo;
+
+init();
