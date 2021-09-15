00b036d21b4736002879b0faae4bfcec372bb095
JunChi <jun@JunChis-MacBook-Pro.local>
2021-09-15 14:54:12 +0800

    added log out function

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ Moralis.serverURL = 'https://t4jve9pvn40e.grandmoralis.com:2053/server'
 
 // enable moralis and connect to web3
 init = async () => {
+    hideElement(userInfo)
     window.web3 = await Moralis.web3.enable();
     initUser();
 }
@@ -10,11 +11,28 @@ init = async () => {
 login = async () => {
     try {
         await Moralis.authenticate();
+        console.log("logged in")
         initUser();
     } catch (error) {
         alert(error)
     }
 }
+
+async function logOut() {
+    await Moralis.User.logOut();
+    hideElement(userInfo)
+    initUser()
+    console.log("logged out");
+  }
+
+openUserInfo = async () => {
+    user = await Moralis.User.current()
+    if (user) {
+        showElement(userInfo)
+    } else {
+        login()
+    }
+} 
 
 initUser = async () => {
     if (await Moralis.User.current()) {
@@ -30,6 +48,12 @@ showElement = (element) => element.style.display = "block"
 
 const userConnectButton = document.getElementById("btnConnect")
 userConnectButton.onclick = login;
+
 const userProfileButton = document.getElementById("btnUserInfo")
+userProfileButton.onclick = openUserInfo;
+
+const userInfo = document.getElementById("userInfo")
+document.getElementById("btnCloseUserInfo").onclick = () => hideElement(userInfo)
+document.getElementById("btnLogout").onclick = logOut
 
 init()
